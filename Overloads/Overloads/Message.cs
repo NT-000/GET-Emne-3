ab1a6e245c7b2913ff1e3be02b69c5550d9cf2a2
NT-000 <nicolai.thue.reier@gmail.com>
2024-11-23 09:58:52 +0100

    update

---
+++ Overloads/Overloads/Message.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Overloads
+{
+    public class Message
+    {
+        public string defCompliment;
+        public string defText;
+        public void PrintWelcomeMessage()
+        {
+            defText = "du er snill";
+            Console.WriteLine($"\nHei og velkommen, {defText}\n");
+
+        }
+
+        public void PrintWelcomeMessage(string compliment)
+        {
+            defCompliment = compliment;
+            Console.WriteLine($"Hei og velkommen, {compliment}\n");
+        }
+    }
+}
---
+++ Overloads/Overloads/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Overloads;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        bool isTrue = true;
+        while (isTrue)
+        {
+            var message = new Message();
+            message.PrintWelcomeMessage();
+            Console.WriteLine("Velg et kompliment!");
+            Console.WriteLine("1: du er kul");
+            Console.WriteLine("2: du er flink!");
+            Console.WriteLine("3: du kler raske briller!");
+            var input = Console.ReadLine();
+            string option1 = "\ndu er kul\n";
+            string option2 = "\ndu er flink!\n";
+            string option3 = "\ndu kler raske briller!\n";
+
+            switch (input)
+            {
+                case "1":
+                    Console.Clear();
+                    message.PrintWelcomeMessage(compliment: option1);
+                    break;
+                case "2":
+                    Console.Clear();
+                    message.PrintWelcomeMessage(compliment: option2);
+                    break;
+                case "3":
+                    Console.Clear();
+                    message.PrintWelcomeMessage(compliment: option3);
+                    break;
+                case "4":
+                    Console.Clear();
+                    Console.WriteLine("Exiting...");
+                    isTrue = false;
+                    break;
+                default:
+                    Console.WriteLine("\nBare 1,2 eller 3!");
+                    Console.Clear();
+                    message.PrintWelcomeMessage(message.defCompliment);
+                    break;
+            }
+        }
+    }
+
+}
