14ed8fc9ac0335dc95ee73d14742d644ad52d2b5
NT-000 <nicolai.thue.reier@gmail.com>
2025-01-14 13:21:46 +0100

    cleanup

---
+++ RandomHobbyGenerator/RandomHobbyGenerator/Program.cs
@@ -0,0 +1,35 @@
+﻿﻿namespace Random_HobbyGenerator
+{
+    internal class RandomHobbies
+    {
+      
+
+
+    static void Run()
+        {
+            Random num = new();
+            string[] Hobbies = { "Darts", "Martial Arts", "Magician", "Fishing", "Whistling", "Swimming", "Running", "Rollerskating", "Shooting" };
+            int randomNum = num.Next(Hobbies.Length);
+            string resultHobbies = Hobbies[randomNum];
+
+            Console.WriteLine("Get a new hobby!");
+            Console.WriteLine("What's your name?");
+            string input = Console.ReadLine();
+            Console.WriteLine($"{input} just discovered the new hobby {resultHobbies}");
+        }
+        static void Main(string[] args)
+        {
+            do
+            {
+                Run();
+                Console.WriteLine("Would you like a new hobby? (y/n)");
+                string answer = Console.ReadLine();
+                if (answer != "y")
+                {
+                    Console.WriteLine("Bye then!");
+                    break;
+                }
+            } while (true);
+        }
+    }
+};
---
+++ ReturnNothing/ReturnNothing/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Return_nothing
+{
+    internal class Program
+    {
+        public static string Text = "Denne stringen returnerer ingenting...";
+        static void Main(string[] args)
+        {
+            printMsg();
+        }
+
+        public static string printMsg()
+        {
+            return Text;
+        }
+    }
+}
